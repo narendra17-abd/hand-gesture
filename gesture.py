bc43961898485867e2b76d075dac8f044aca905d
Narendra <157557161+narendra17-abd@users.noreply.github.com>
2024-02-26 19:01:45 +0530

    Add files via upload

---
+++ gesture.py
@@ -0,0 +1,26 @@
+import serial
+import pyautogui                                   
+
+Arduino_Serial = serial.Serial('com5',9600)       
+ 
+while 1:
+    incoming_data = str (Arduino_Serial.readline()) 
+    print incoming_data                           
+     
+
+    if 'next' in incoming_data:                    
+        pyautogui.hotkey('ctrl', 'pgdn')          
+        
+    if 'previous' in incoming_data:                
+        pyautogui.hotkey('ctrl', 'pgup')
+
+
+    if 'down' in incoming_data:                    
+        #pyautogui.press('down')                
+        pyautogui.scroll(-100) 
+         
+    if 'up' in incoming_data:                      
+        #pyautogui.press('up')                      
+        pyautogui.scroll(100)
+        
+    incoming_data = "";                            
